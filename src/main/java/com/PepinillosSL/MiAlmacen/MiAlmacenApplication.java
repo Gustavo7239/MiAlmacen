1c248ac46f8caf2afda2bf3fde365399a64bcb28
Gustavo <GustavoCQ.DAM@Gmail.com>
2024-01-23 09:28:38 +0100

    First Commit

---
+++ src/main/java/com/PepinillosSL/MiAlmacen/MiAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.PepinillosSL.MiAlmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiAlmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PepinillosSL/MiAlmacen/MiAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PepinillosSL.MiAlmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
